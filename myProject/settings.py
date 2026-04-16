1e859dbe30569ba3337ad0114e495b0f5f994044
Pallavi Kandikanti <pallavikandikantica@gmail.com>
2026-04-16 00:14:25 -0400

    chnages

--- myProject/settings.py
+++ myProject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-7+(c##gy9ms9!&na*@w*!7+i00&in($tq(8z46a*a4esm032s_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
